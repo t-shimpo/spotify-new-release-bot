a9acfb4ddd3bc8c252bc5fc2d04e41750bdd0c4f
t-shimpo <zanjibaru7@gmail.com>
2022-12-17 15:10:48 +0900

    LINE BotでHello Worldを送信する

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func main() {
@@ -13,9 +13,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	secret := os.Getenv("LINE_CHANNEL_SECRET")
-	token := os.Getenv("LINE_CHANNEL_TOKEN")
 
-	fmt.Println(secret)
-	fmt.Println(token)
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage("Hello World")
+
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
