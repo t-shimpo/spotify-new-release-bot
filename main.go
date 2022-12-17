a1299155f69f3b95087158d33aea230d5b4bc8ad
t-shimpo <zanjibaru7@gmail.com>
2022-12-17 17:08:36 +0900

    Spotify APIで新作リリース情報を取得する

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+
 	"github.com/joho/godotenv"
-	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/zmb3/spotify/v2"
+	"golang.org/x/oauth2/clientcredentials"
+	// "github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 func main() {
@@ -14,17 +20,37 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	bot, err := linebot.New(
-		os.Getenv("LINE_CHANNEL_SECRET"),
-		os.Getenv("LINE_CHANNEL_TOKEN"),
-	)
+	// bot, err := linebot.New(
+	// 	os.Getenv("LINE_CHANNEL_SECRET"),
+	// 	os.Getenv("LINE_CHANNEL_TOKEN"),
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// message := linebot.NewTextMessage("Hello World")
+
+	// if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+
+	res, err := client.NewReleases(ctx, spotify.Country("JP"), spotify.Limit(10))
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	message := linebot.NewTextMessage("Hello World")
-
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(res)
 }
