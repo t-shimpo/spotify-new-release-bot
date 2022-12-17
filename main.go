040906e9f40098d090eb93079ed8ad7a70b705f6
t-shimpo <zanjibaru7@gmail.com>
2022-12-17 10:50:36 +0900

    環境変数を出力する

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	secret := os.Getenv("LINE_CHANNEL_SECRET")
+	token := os.Getenv("LINE_CHANNEL_TOKEN")
+
+	fmt.Println(secret)
+	fmt.Println(token)
 }
